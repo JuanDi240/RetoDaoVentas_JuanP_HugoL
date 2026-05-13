d939a2e697cb5f293374e573883db3fe6232ff60
vhugolm2023 <victo@Vivi>
2026-05-13 13:23:15 +0200

    Persona codigo serializable

--- Reto_Dao_ventas/src/serie/Persona.java
+++ Reto_Dao_ventas/src/serie/Persona.java
@@ -1,5 +1,24 @@
 package serie;
 
-public class Persona {
+import java.io.Serializable;
+
+public class Persona implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	protected int id;
+	protected String dni;
+	protected String nombre;
+	public Persona(int id, String dni, String nombre) {
+		super();
+		this.id = id;
+		this.dni = dni;
+		this.nombre = nombre;
+	}
+	@Override
+	public String toString() {
+		return "Persona [id=" + id + ", dni=" + dni + ", nombre=" + nombre + "]";
+	}
+	
+	
 
 }
