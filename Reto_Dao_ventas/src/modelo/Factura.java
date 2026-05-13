5f9e76e03971f3a57551a0c8d2c17f97c0ffa7d9
WiizMark <1DAM@A11-PC09>
2026-05-13 13:55:09 +0200

    Comit actualizacion de varios archivos:
    Factura.java, LineaFactura.java, Producto.java, ProductoDAO.java

--- Reto_Dao_ventas/src/modelo/Factura.java
+++ Reto_Dao_ventas/src/modelo/Factura.java
@@ -1,5 +1,90 @@
 package modelo;
 
+import java.time.LocalDate;
+
 public class Factura {
+	int id;
+	LocalDate fecha;
+	int id_cliente;
+	int id_empleado;
+	double subtotal; 
+	double iva;
+	double total;
+	
+	public Factura() {
+		super();
+	}
+	
+	public Factura(LocalDate fecha, int id_cliente, int id_empleado, double subtotal, double iva, double total) {
+		super();
+		this.fecha = fecha;
+		this.id_cliente = id_cliente;
+		this.id_empleado = id_empleado;
+		this.subtotal = subtotal;
+		this.iva = iva;
+		this.total = total;
+	}
+	
+	public Factura(int id, LocalDate fecha, int id_cliente, int id_empleado, double subtotal, double iva,
+			double total) {
+		super();
+		this.id = id;
+		this.fecha = fecha;
+		this.id_cliente = id_cliente;
+		this.id_empleado = id_empleado;
+		this.subtotal = subtotal;
+		this.iva = iva;
+		this.total = total;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public LocalDate getFecha() {
+		return fecha;
+	}
+	public void setFecha(LocalDate fecha) {
+		this.fecha = fecha;
+	}
+	public int getId_cliente() {
+		return id_cliente;
+	}
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+	public int getId_empleado() {
+		return id_empleado;
+	}
+	public void setId_empleado(int id_empleado) {
+		this.id_empleado = id_empleado;
+	}
+	public double getSubtotal() {
+		return subtotal;
+	}
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+	public double getIva() {
+		return iva;
+	}
+	public void setIva(double iva) {
+		this.iva = iva;
+	}
+	public double getTotal() {
+		return total;
+	}
+	public void setTotal(double total) {
+		this.total = total;
+	}
+	
+	@Override
+	public String toString() {
+		return "Factura [id=" + id + ", fecha=" + fecha + ", id_cliente=" + id_cliente + ", id_empleado=" + id_empleado
+				+ ", subtotal=" + subtotal + ", iva=" + iva + ", total=" + total + "]";
+	}
+
 
 }
--- Reto_Dao_ventas/src/modelo/Producto.java
+++ Reto_Dao_ventas/src/modelo/Producto.java
@@ -1,5 +1,69 @@
 package modelo;
 
 public class Producto {
+	int id;
+	String nombre;
+	double precio;
+	int stock;
+	
+	public Producto() {
+		super();
+	}
+
+	public Producto(String nombre, double precio, int stock) {
+		super();
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+	}
+
+	public Producto(int id, String nombre, double precio, int stock) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.precio = precio;
+		this.stock = stock;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public int getStock() {
+		return stock;
+	}
+
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [id=" + id + ", nombre=" + nombre + ", precio=" + precio + ", stock=" + stock + "]";
+	}
+
+	
 
 }
+
+	
