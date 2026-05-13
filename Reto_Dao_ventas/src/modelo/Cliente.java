a5b1a1493118ec6eff8d7826bccf752149e1cfcc
vhugolm2023 <victo@Vivi>
2026-05-13 13:38:38 +0200

    Cliente y empleado, con herencia de persona

--- Reto_Dao_ventas/src/modelo/Cliente.java
+++ Reto_Dao_ventas/src/modelo/Cliente.java
@@ -1,5 +1,36 @@
 package modelo;
 
-public class Cliente {
+public class Cliente extends Persona {
+	private int id;
+	private String direccion;
+	public Cliente() {
+		super();
+	}
+	public Cliente(String direccion) {
+		super();
+		this.direccion = direccion;
+	}
+	public Cliente(int id, String direccion) {
+		super();
+		this.id = id;
+		this.direccion = direccion;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", direccion=" + direccion + "]";
+	}
+	
 
 }
