f6a3a734fd21287fe0b21bc3a78d89da9bfeb240
vhugolm2023 <victo@Vivi>
2026-05-15 12:14:19 +0200

    correcion del cliente 3

--- Reto_Dao_ventas/src/modelo/Cliente.java
+++ Reto_Dao_ventas/src/modelo/Cliente.java
@@ -1,32 +1,35 @@
 package modelo;
 
-import serie.Persona;
-
 public class Cliente extends Persona {
-	private static final long serialVersionUID = 1L;
+	
 	private String direccion;
-	public Cliente(String dni, String nombre) {
-		super(dni, nombre);
+	
+
+	public Cliente() {
+		super();
 	}
-	public Cliente(String dni, String nombre, String direccion) {
-		super(dni, nombre);
+
+
+	public Cliente(String direccion) {
+		super();
 		this.direccion = direccion;
 	}
+
+
 	public String getDireccion() {
 		return direccion;
 	}
+
+
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
-	public static long getSerialversionuid() {
-		return serialVersionUID;
-	}
+
+
 	@Override
 	public String toString() {
-		return "Cliente [direccion=" + direccion + ", dni=" + dni + ", nombre=" + nombre + "]";
+		return "Cliente [direccion=" + direccion + ", id=" + id + ", dni=" + dni + ", nombre=" + nombre + "]";
 	}
-	
-	
 	
 	
 
