5e433dd8e75c17e0e987e35153f60c72bcae7f4f
vhugolm2023 <victo@Vivi>
2026-05-15 08:55:50 +0200

    Solucion con la base de datos.

--- Reto_Dao_ventas/src/app/MainGeneral.java
+++ Reto_Dao_ventas/src/app/MainGeneral.java
@@ -1,9 +1,21 @@
 package app;
 
+import dao.ClienteDAO;
+import modelo.Cliente;
+
 public class MainGeneral {
 
 	public static void main(String[] args) {
-		System.out.println("Negritas");
+		//1.	Muestra todos los clientes, selecciona uno escribiendo el id y muestra sus datos y sus facturas.
+		//2.	Muestra todos los empleados y seleccionando un id muestra las facturas que ha emitido.
+		
+		ClienteDAO cDAO= new ClienteDAO();
+		
+		for (Cliente c : cDAO.obtenerTodos()) {
+			System.out.println(c);
+		}
+
+		
 
 	}
 
