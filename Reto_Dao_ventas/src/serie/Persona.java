d54da2874291844b5ecb841fe480f3d4b0bca254
vhugolm2023 <victo@Vivi>
2026-05-15 11:56:33 +0200

    serializable correcion codigo.

--- Reto_Dao_ventas/src/serie/Persona.java
+++ Reto_Dao_ventas/src/serie/Persona.java
@@ -5,19 +5,20 @@ import java.io.Serializable;
 public class Persona implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	protected int id;
+	
 	protected String dni;
 	protected String nombre;
 	public Persona(int id, String dni, String nombre) {
 		super();
-		this.id = id;
+		
 		this.dni = dni;
 		this.nombre = nombre;
 	}
 	@Override
 	public String toString() {
-		return "Persona [id=" + id + ", dni=" + dni + ", nombre=" + nombre + "]";
+		return "Persona [dni=" + dni + ", nombre=" + nombre + "]";
 	}
+	
 	
 	
 
