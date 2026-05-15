3244d6a3307834905be02a429d6df8b99d26fbb1
vhugolm2023 <victo@Vivi>
2026-05-15 09:07:36 +0200

    Main mostrar todos

--- Reto_Dao_ventas/src/app/MainGeneral.java
+++ Reto_Dao_ventas/src/app/MainGeneral.java
@@ -9,11 +9,14 @@ public class MainGeneral {
 		//1.	Muestra todos los clientes, selecciona uno escribiendo el id y muestra sus datos y sus facturas.
 		//2.	Muestra todos los empleados y seleccionando un id muestra las facturas que ha emitido.
 		
+
+		
 		ClienteDAO cDAO= new ClienteDAO();
 		
 		for (Cliente c : cDAO.obtenerTodos()) {
 			System.out.println(c);
 		}
+		
 
 		
 
